c5cdcaaa36aae4a33dadb9e3c770fd754a5ba1fc
grzesiowski1223 <84310323+grzesiowski1223@users.noreply.github.com>
2025-01-11 00:48:22 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+echo !!!EDIT THIS FILE WITH WHATEVER TEXT EDITOR YOU USE TO SETUP!!!
+#In brackets type address of your server or address@user etc...
+
+$IPADDRESS=[INSERT SERVER IP ADDRESS]
+
+
+
+#  _   _              _ _   _      _                      _    
+# | \ | |            | | \ | |    | |                    | |   
+# |  \| | ___ _ __ __| |  \| | ___| |___      _____  _ __| | __
+# | . ` |/ _ \ '__/ _` | . ` |/ _ \ __\ \ /\ / / _ \| '__| |/ /
+#| |\  |  __/ | | (_| | |\  |  __/ |_ \ V  V / (_) | |  |   < 
+# |_| \_|\___|_|  \__,_|_| \_|\___|\__| \_/\_/ \___/|_|  |_|\_\
