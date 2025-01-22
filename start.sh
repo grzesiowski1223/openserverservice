f41a3eb1cebc0d1750891421b6d1bc9155f64619
grzesiowski1223 <84310323+grzesiowski1223@users.noreply.github.com>
2025-01-22 11:46:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ echo               ────────────▀▀▀▀▀▀▀
 read -r -p "Czy chcesz polaczyc sie z serverem NerdNetwork? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
+source <filename>
+ssh $IPADDRESS
     # zrob source i ssh 
     # https://askubuntu.com/questions/367136/how-do-i-read-a-variable-from-a-file
         echo 5
