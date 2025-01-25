022199793ad042f20c8fca5ab1d86c5897f48386
grzesiowski1223 <84310323+grzesiowski1223@users.noreply.github.com>
2025-01-25 21:35:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ echo               ────────────▀▀▀▀▀▀▀
 
 echo PLEASE READ README!!!!
 
+IPADDRESS=for_example_192.168.0.1
+
 read -r -p "Are you want connect to the server? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
