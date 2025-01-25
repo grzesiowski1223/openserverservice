b75e1bb8595ca16eee5b9c4b56ef1bd224fa5c75
grzesiowski1223 <84310323+grzesiowski1223@users.noreply.github.com>
2025-01-25 21:40:44 +0100

    Total rework of start.sh
    Rework of old version

--- start.sh
+++ start.sh
@@ -1,22 +1,23 @@
-echo               ─────▄▄────▄▀▀█▀▀▀▀▄
-echo               ──▄▀▀──▀▀▄▄█▄▄█────█
-echo               ▄▀─────────█──█────█
-echo               ────────────▀▀▀▀▀▀▀            
+#!/bin/bash
 
-echo PLEASE READ README!!!!
+echo "─────▄▄────▄▀▀█▀▀▀▀▄"
+echo "──▄▀▀──▀▀▄▄█▄▄█────█"
+echo "▄▀─────────█──█────█"
+echo "────────────▀▀▀▀▀▀▀"
 
-IPADDRESS=for_example_192.168.0.1
+echo "PLEASE READ README!!!!"
 
-read -r -p "Are you want connect to the server? [y/N] " response
+# To connect with your server enter ip address here
+IPADDRESS="192.168.0.1"
+
+read -r -p "Do you want to connect to the server at $IPADDRESS? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-IPADDRESS=!!Enter here ip of the server!!
-ssh $IPADDRESS
-        echo 5
+        ssh "$IPADDRESS"
+        echo "Connection closed."
         ;;
     *)
-        echo Exit OSS
+        echo "Exiting..."
         exit
         ;;
 esac
-
